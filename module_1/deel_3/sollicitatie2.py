7d19ab45be17db4971dcd3cd9f24afe2aef943bc
YilmazNL2005 <99069863@mydavinci.nl>
2023-10-05 16:54:18 +0200

    sollicitatie_begin

---
+++ module_1/deel_3/sollicitatie2.py
@@ -0,0 +1,47 @@
+# Bepaal met behulp van een aantal vragen in jouw sollicitatie.py 
+# of de kandidaat mag gaan solliciteren naar de functie.
+
+# Stel eerst alle vragen en ga daarna pas beoordelen aan de hand van de gegeven antwoorden 
+# of de kandidaat mag solliciteren.
+
+# De kandidaat moet wel voldoen aan de volgende criteria:
+
+# In bezit van een geldig Vrachtwagen rijbewijs. 
+# Een echte circusdirecteur rijdt ook de grootste circuswagen zelf!
+# In bezit van een hoge hoed.
+# Is zwaarder dan 90 kg en lichter dan 120 kg
+# Is langer dan 150 cm en korter dan 220 cm
+# Heeft Certificaat “Overleven met gevaarlijk personeel”
+# Heeft:
+# minimaal 4 jaar praktijkervaring met dieren-dressuur OF
+# minimaal 5 jaar ervaring met jongleren OF
+# minimaal 3 jaar praktijkervaring met acrobatiek.
+# Let op: Vraag de jaren ervaring afzonderlijk voor alle kunsten: dieren-dressuur, 
+# jongleren én acrobatiek. Dus het programma stelt hierover precies 3 vragen!
+
+
+
+# Let op: De vragen met getallen mogen de kritieke grens (zoals 4 jaar, of 150 cm) 
+# van een criterium niet verklappen.  Anders krijg je geen eerlijk antwoord van de kandidaten.
+
+# Dus je vraagt bijvoorbeeld:
+
+# 'Wat is uw lichaamsgewicht?’ in plaats van 'Weegt  u zwaarder dan 90 kg en lichter dan 120 kg? J/N’
+# ‘Hoeveel jaar praktijkervaring heeft u met dit-of-dat ?’ in plaats van 
+# 'Heeft u meer dan 4 jaar praktijkervaring met dit-of-dat? J/N’
+
+# Let op: Eerst alle vragen stellen, daarna pas de geschiktheid berekenen 
+# op basis van de gegeven antwoorden! 
+# Pas na het berekenen van de geschiktheid geeft u de uitslag van de sollicitatie aan de invuller.
+
+
+# Gebruik zeker de volgende technieken:
+
+# if-elif-else
+# gebruik and en or  in condities
+# Maak constanten voor alle getallen die je verder gebruikt in de code. Bijvoorbeeld:
+
+# Een constante in Python is technisch gezien een variabele 
+# en heeft een naam in hoofdletters wat aangeeft dat de waarde niet mag veranderen 
+# gedurende uitvoering van het programma.
+
