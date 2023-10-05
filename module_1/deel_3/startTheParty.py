4b1f9e4d27e7446787ae1cdc0034e429a78dbf32
YilmazNL2005 <99069863@mydavinci.nl>
2023-10-05 16:27:22 +0200

    gastheer_uitbreiding_party

--- module_1/deel_3/startTheParty.py
+++ module_1/deel_3/startTheParty.py
@@ -6,6 +6,11 @@ chips = True
 aanwezig_gastheer = input("Is er een gastheer op de party? ")
 if aanwezig_gastheer == "nee":
     gastheer = False
+elif aanwezig_gastheer == "ja":
+    naam_gastheer = input("Hoe heet de gastheer? ")
+    if naam_gastheer == "oorschot" or naam_gastheer == "":
+        gastheer = False
+        gasten = False
 aanwezig_gasten = input("Zijn er gasten op de party? ")
 if aanwezig_gasten == "nee":
     gasten = False
@@ -16,7 +21,7 @@ aanwezig_chips = input("Is er chips op de party? ")
 if aanwezig_chips == "nee":
     chips = False
 
-if gastheer and chips and drank == True or gasten and chips and drank == True or gastheer and drank == True :
+if gastheer and chips and drank == True or gasten and chips and drank == True or gastheer and drank == True or naam_gastheer == "yilmaz":
     print('Start the Party')
 else:
     print('No Party')
