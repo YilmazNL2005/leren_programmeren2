9b75d5e72b7cf8f8b7c7d0121b12c4e350122681
YilmazNL2005 <99069863@mydavinci.nl>
2023-10-05 16:43:45 +0200

    vereisten_minimaal_maximaal_gasten_toegevoegd_aan_Party

--- module_1/deel_3/startTheParty.py
+++ module_1/deel_3/startTheParty.py
@@ -14,6 +14,10 @@ elif aanwezig_gastheer == "ja":
 aanwezig_gasten = input("Zijn er gasten op de party? ")
 if aanwezig_gasten == "nee":
     gasten = False
+elif aanwezig_gasten == "ja":
+    aantal_gasten = int(input("Hoeveel gasten zijn er op de party? "))
+    if aantal_gasten == 0:
+        aanwezig_gasten = False
 aanwezig_drank = input("Is er drank op de party? ")
 if aanwezig_drank == "nee":
     drank = False
@@ -21,7 +25,14 @@ aanwezig_chips = input("Is er chips op de party? ")
 if aanwezig_chips == "nee":
     chips = False
 
-if gastheer and chips and drank == True or gasten and chips and drank == True or gastheer and drank == True or naam_gastheer == "yilmaz":
+if gastheer and chips and drank == True or gasten and chips and drank == True and aantal_gasten >= 4 and aantal_gasten <= 20 or gastheer and drank == True or naam_gastheer == "yilmaz":
     print('Start the Party')
 else:
     print('No Party')
+
+# In het kort wanneer een feest kan beginnen
+
+# - als er een gastheer, chips en drank aanwezig is.
+# - als er minimaal 4 en maximaal 20 gasten, chips en drank aanwezig is.
+# - als er een gastheer en drank aanwezig is.
+# - als de naam van de gastheer yilmaz is. 
