7d64e0efadd1032684623ba26c0695e2839f2691
YilmazNL2005 <99069863@mydavinci.nl>
2023-10-31 16:15:56 +0100

    pizzaCalculator_verbeterd

--- module_1/deel_2/pizzaCalculator.py
+++ module_1/deel_2/pizzaCalculator.py
@@ -2,14 +2,15 @@ PRIJS_KLEIN = 3.99
 PRIJS_MIDDEL = 6.99
 PRIJS_GROOT = 11.99
 
-def get_int(txtInvoer):
+def get_int(aantal_opgeven):
     while True:
         try:
-            return int(input(txtInvoer))
+            return int(input(aantal_opgeven))
         except ValueError:
             print("Dit is geen getal, maar een woord of letter.")
+            print("Het opgegeven aantal moet opnieuw worden gegeven. Vul een heel getal in om je bestelling te plaatsen")
 
-    
+
 aantal_klein = get_int("Hoeveel kleine pizza's wil je? ")
 aantal_middel = get_int("Hoeveel middel pizza's wil je? ")
 aantal_groot = get_int("Hoeveel groot pizza's wil je? ")
@@ -22,6 +23,6 @@ print(f" Kleine pizza's  {aantal_klein} stuks.        Prijs    {PRIJS_KLEIN * aa
 print(f" Middel pizza's  {aantal_middel} stuks.        Prijs    {PRIJS_MIDDEL * aantal_middel} euro.")
 print(f" Grote  pizza's  {aantal_groot} stuks.        Prijs    {PRIJS_GROOT * aantal_groot} euro. \n")
 
-print(f" Totaal                                   {totaal_prijs} euro. ")
+print(f" Totaal                                   {round(totaal_prijs, 2)} euro. ")
 
 print("_____________________________________________________\n")
