c747648d738c0420c68d95e52d824df577300e76
YilmazNL2005 <99069863@mydavinci.nl>
2023-11-03 11:37:55 +0100

    verbeteringen

--- module_1/deel_4/ice_bugs.py
+++ module_1/deel_4/ice_bugs.py
@@ -4,8 +4,8 @@ def convertToEuroText (amount):
 SMALL_PRICE = 1.25
 MEDIUM_PRICE = 2.10
 #Varibelen moet je niet hergebruiken.
-amount = float(input('Hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(SMALL_PRICE))))
-amount_a = float(input('En hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(MEDIUM_PRICE))))
+amount = int(input('Hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(SMALL_PRICE))))
+amount_a = int(input('En hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(MEDIUM_PRICE))))
 totalPrice = amount * SMALL_PRICE + amount_a * MEDIUM_PRICE
 
 print('Dat is dan {} totaal'.format(convertToEuroText(totalPrice)))
