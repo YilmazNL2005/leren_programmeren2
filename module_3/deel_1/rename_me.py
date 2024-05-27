365cb3ddf102234efe7ba307d52aa7ef6e0bbaea
YilmazNL2005 <99069863@mydavinci.nl>
2024-05-27 16:13:30 +0200

    rename_python_done

--- module_3/deel_1/rename_me.py
+++ module_3/deel_1/rename_me.py
@@ -5,27 +5,29 @@ def string_changer(woord_zin:str) -> str:
     string_lijst = woord_zin.split() # De split zorgt ervoor dat de string wordt opgesplitst in een lijst. Dus elk woord is een item met een eigen index.
     lijst_omkeren = string_lijst[::-1] # Lijst wordt omgekeerd
     omgekeerde_string = ' '.join(lijst_omkeren) # Na elk item in een list wordt er met een join in dit geval steeds " " geplaatst.
-    return print(omgekeerde_string)
+    return print(omgekeerde_string) # Resultaat wordt getoond.
 
-def kosmische_koekjesmix(galactische_snoepjes:str) -> int:
-    planetair_taartje = set(galactische_snoepjes)
-    whatchamacallit = len(planetair_taartje)
-    return whatchamacallit
+def string_lengte(gegeven_woord:str) -> int: # Deze functie geeft aan hoeveel unieke letters in een string zitten.
+    dubbele_letter_vw = set(gegeven_woord) # Set zorgt ervoor dat elke letter max 1 keer voorkomt. De dubbele verwijderd hij.
+    aantal_soorten_letters = len(dubbele_letter_vw) # Checkt hoeveel letters nu in de string zitten.
+    return print(aantal_soorten_letters) # Aantal letters wordt getoond.
 
-def ruimte_hamsterwiel(planetair_taartje:str) -> float:
-    wobbelwobbel = planetair_taartje.split()
-    
-    blork = 0
-    for snorkelwagen in wobbelwobbel:
-        blork += len(snorkelwagen)
+def gemiddelde_letter(woord_of_zin:str) -> float:
+    lijst_woorden = woord_of_zin.split() # De split zorgt ervoor dat de string wordt opgesplitst in een lijst. Dus elk woord is een item met een eigen index.
+    totaal_letters = 0
+    for woord in lijst_woorden: # For loop loopt door de lijst heen.
+        totaal_letters += len(woord) # Telt het aantal letters van het item uit de lijst en voegt dat aantal bij het totaal. 
 
-    bizarro_matrix = blork / len(wobbelwobbel)
-    return bizarro_matrix
+    letters_per_woord = totaal_letters / len(lijst_woorden) # Hier wordt het gemiddelde aantal letters dat per woord wordt gebruikt berekend.
+    return print(letters_per_woord) # Deze return laat zien hoeveel letters ieder woord gemiddeld krijgt.
 
-def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None:
-    for zwabber_krakeling in range(1, parallelle_tosti+1):
-        laser_sandwich = zwabber_krakeling * infinity_pizza
-        print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
+def tafel_van(tafel:int, tot_max:int=10) -> None:
+    for aantal_keer in range(1, tot_max+1): # For loop die zorgt dat het aantal keer dat de tafel van ... tot 10 wordt gedaan. 
+        berekening_tafel = aantal_keer * tafel # Dit de berekening per aantal tafel. 
+        print(f'{aantal_keer} x {tafel} = {berekening_tafel}') # De print toont de berekeningen aan de gebruiker. Door de for loop van regel 25 zal dit in dit geval 10 keer uitgevoerd worden.
 
 is_even(2)
 string_changer("Hallo ik ben Yilmaz")
+string_lengte("Hallo wereld")
+gemiddelde_letter("Dit is een voorbeeldzin")
+tafel_van(50)
--- module_3/deel_1/test.py
+++ module_3/deel_1/test.py
@@ -1,4 +1,24 @@
-lijstje = ["appel", "banaan", "peer", "watermeloen", "bosbes", "citroen", "druif"]
-print(lijstje)
-onbekende_werking = lijstje[1:6:2]
-print(onbekende_werking)
+# lijstje = ["appel", "banaan", "peer", "watermeloen", "bosbes", "citroen", "druif"]
+# print(lijstje)
+# onbekende_werking = lijstje[1:6:2]
+# print(onbekende_werking)
+
+zin = "Dit is een voorbeeldzin"
+woorden = zin.split()
+aantal = 0
+for woord in woorden:
+    aantal += len(woord)
+    print(woord)
+print(aantal)
+print(len(woorden))
+halveren = aantal / len(woorden)
+print(halveren)
+
+# def functie(woord_of_zin:str) -> float:
+#     wobbelwobbel = woord_of_zin.split() # De split zorgt ervoor dat de string wordt opgesplitst in een lijst. Dus elk woord is een item met een eigen index.
+#     blork = 0
+#     for snorkelwagen in wobbelwobbel: # For loop
+#         blork += len(snorkelwagen)
+
+#     bizarro_matrix = blork / len(wobbelwobbel)
+#     return print(bizarro_matrix)
