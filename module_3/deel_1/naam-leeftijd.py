2c7fb4714707f1194589613aac2edf2a09457497
YilmazNL2005 <99069863@mydavinci.nl>
2024-06-11 11:49:47 +0200

    naam-leeftijd is nu een betere versie

--- module_3/deel_1/naam-leeftijd.py
+++ module_3/deel_1/naam-leeftijd.py
@@ -1,7 +1,7 @@
-def vraag_naam(vraag_n):
-    return vraag_n
-def vraag_leeftijd(vraag_l):
-    return vraag_l
+def naam_leeftijd(name, age, info):
+    info["naam"] = name
+    info["leeftijd"] = age
+    return info
 
 naam = input("Wat is je naam? ")
 leeftijd = int(input("Wat is je leeftijd in jaren? "))
@@ -9,11 +9,7 @@ leeftijd = int(input("Wat is je leeftijd in jaren? "))
 informatie =    {"naam" : None,
                 "leeftijd" : None}
 
-naam_terug = vraag_naam(naam)
-leeftijd_terug = vraag_leeftijd(leeftijd)
-# print(naam)
-# print(leeftijd_terug)
-informatie["naam"] = naam_terug
-informatie["leeftijd"] = leeftijd_terug
-# print(informatie) 
-print(f"{informatie['naam']} is {informatie['leeftijd']} jaar oud.")
+teruggave = naam_leeftijd(naam, leeftijd, informatie)
+print(teruggave)
+
+print(f"{teruggave['naam']} is {teruggave['leeftijd']} jaar oud.")
--- module_3/deel_1/test.py
+++ module_3/deel_1/test.py
@@ -1,4 +1,15 @@
-def function(l:list):
-    print(l)
-function(6)
-function()
+def naam_leeftijd(name, age, info):
+    info["naam"] = name
+    info["leeftijd"] = age
+    return info
+
+naam = input("Wat is je naam? ")
+leeftijd = int(input("Wat is je leeftijd in jaren? "))
+
+informatie =    {"naam" : None,
+                "leeftijd" : None}
+
+teruggave = naam_leeftijd(naam, leeftijd, informatie)
+print(teruggave)
+
+print(f"{teruggave['naam']} is {teruggave['leeftijd']} jaar oud.")
